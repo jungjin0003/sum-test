4f191ce79d413106d35f6f3bef295b2d51f619f6
jungjin0003 <jungjin00031@gmail.com>
2021-07-07 03:41:27 -0400

    Add sum modules

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+// file : sum.cpp
+#include "sum.h"
+
+int sum(int n)
+{
+	int result = 0;
+	for (int i = 0; i <= n; i++)
+	{
+		result += i;
+	}
+}
---
+++ sum.h
@@ -0,0 +1,5 @@
+// file : sum.h
+#pragma once
+
+int sum(int n);
+
